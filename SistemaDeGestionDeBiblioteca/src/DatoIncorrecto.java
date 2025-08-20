1d02581e8f2bd24ee354809217f3efd48775eb73
Thiago Viccichi <viccichithiago@gmail.com>
2025-08-20 19:07:46 -0300

    Agregacion de las clases Usuario y la Excepcion que vamos a manejar

---
+++ SistemaDeGestionDeBiblioteca/src/DatoIncorrecto.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+public class DatoIncorrecto extends RuntimeException {
+  public DatoIncorrecto(String message) {
+    super(message);
+  }
+}
---
+++ SistemaDeGestionDeBiblioteca/src/Usuario.java
@@ -0,0 +1,51 @@
+import java.util.HashSet;
+
+public class Usuario {
+    //Atributos
+    private String nombre, apellido;
+    private int dni;
+    // private HashSet<Prestamo> listaPrestamos = new HashSet<>();
+
+    //Constructor
+    public Usuario(String nombre, String apellido, int dni) {
+        setNombre(nombre);
+        setApellido(apellido);
+        setDni(dni);
+    }
+    //Setters
+    public void setNombre(String nombre) {
+        if (nombre == null || nombre.isBlank()) {
+            throw new DatoIncorrecto("El nombre no puede ser nulo o estar en blanco");
+        }
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        if (apellido == null || apellido.isBlank()) {
+            throw new DatoIncorrecto("El apellido no puede ser nulo o estar en blanco");
+        }
+        this.apellido = apellido;
+    }
+
+    public void setDni(int dni) {
+        if (dni <= 0 || dni >= 100000000) {
+            throw new DatoIncorrecto("El dni que insertaste no puede ser un numero negativo o excederse del limite de rango(10)");
+        }
+        this.dni = dni;
+    }
+    //GETTERS
+
+    public String getNombre() {
+        return this.nombre;
+    }
+    public String getApellido(){return this.apellido;}
+    public int getDni(){return this.dni;}
+
+    //Métodos
+//    public void solicitarPrestamos(Prestamo libro){
+//        listaPrestamos.add(libro);
+//    };
+//    public void eliminarPrestamos(Prestamo libro){
+//        listaPrestamos.remove(libro);
+//    };
+}

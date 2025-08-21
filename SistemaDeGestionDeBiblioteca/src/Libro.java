1ae1d3cb64c84d7b4a1297fa746456b0393e1f59
Thiago Viccichi <viccichithiago@gmail.com>
2025-08-21 17:09:04 -0300

    Agregamos la clase Libro al proyecto

---
+++ SistemaDeGestionDeBiblioteca/src/Libro.java
@@ -0,0 +1,52 @@
+public class Libro {
+    //Atributos
+    private String autor,nombre,reseña,genero;
+    //Constructor
+    public Libro(String autor,String nombre,String reseña,String genero){
+        setAutor(autor);
+        setNombre(nombre);
+        setReseña(reseña);
+        setGenero(genero);
+    }
+    //setters
+
+    private void setAutor(String autor) {
+        if(autor== null || autor.isBlank()){
+            throw new DatoIncorrecto("El libro no puede no tener asignado un author");
+        }
+        this.autor = autor;
+    }
+    private void setNombre(String nombre) {
+        if(nombre== null || nombre.isBlank()){
+            throw new DatoIncorrecto("El libro no puede no tener asignado un nombre");
+        }
+        this.nombre = nombre;
+    }
+    public void setReseña(String reseña) {
+        if(reseña== null || reseña.isBlank()){
+            throw new DatoIncorrecto("El libro no puede no tener asignada una reseña");
+        }
+        this.reseña = reseña;
+    }
+    public void setGenero(String genero) {
+        if(genero== null || genero.isBlank()){
+            throw new DatoIncorrecto("El libro no puede no tener asignado un genero");
+        }
+        this.genero = genero;
+    }
+    //getters
+
+    public String getAutor() {
+        return this.autor;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+    public String getReseña(){
+        return this.reseña;
+    }
+    public String getGenero(){
+        return this.genero;
+    }
+}
